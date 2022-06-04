541ff5a4f0cea43e66fbfcdf85c8000d63413947
Carlos1200991 <1200991@isep.ipp.pt>
2022-06-04 15:50:54 +0100

    added zombie script

---
+++ GameJam2022/Assets/Scripts/EnemyMovment.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyMovment : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+
+    private Transform target;
+    private float movementCooldown = 2;
+    private float cooldown = 0;
+    Vector3 lastPosition;
+
+    void Update ()
+    {
+        if (target != null)
+        {
+            transform.position =
+                Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            if(cooldown <= 0) {
+                lastPosition = new Vector2(Random.Range(-90, 90), Random.Range(-90, 90));
+                transform.position = Vector3.MoveTowards(transform.position, lastPosition, speed * Time.deltaTime); 
+                    
+                cooldown = movementCooldown;
+            }
+            else
+            {
+                cooldown -= Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, lastPosition, speed * Time.deltaTime);
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Here");
+            target = col.transform;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit an enemy");
+        }
+    }
+
+    public void Die()
+    {
+        Manager.instance.enemyDeath();
+        Destroy(gameObject);
+    }
+}
---
+++ GameJam2022/Assets/Scripts/Manager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Manager : MonoBehaviour
+{
+	
+		
+		
+	public static Manager instance = null;
+	public GameObject enemy;
+	
+	
+	private int score = 0;
+	private int score_per_second = 500;
+	private int score_per_enemy = 20000;
+
+	void Awake()
+	{
+        if (instance == null)
+
+            instance = this;
+
+        else if (instance != this)
+
+            Destroy(gameObject);	
+		
+		DontDestroyOnLoad(gameObject);
+
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		score = 0;
+		InvokeRepeating("Spawn_Enemies", 5.0f, 10f);
+	}
+
+	private void Update()
+	{
+		score = (int)(Time.deltaTime * score_per_second);
+	}
+
+	public void enemyDeath()
+	{
+		score += score_per_enemy;
+	}
+
+	private void Spawn_Enemies()
+	{
+		//Not sure yet how this method should be done
+	}
+}
+
