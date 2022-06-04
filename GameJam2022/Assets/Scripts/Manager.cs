585945e49fc265f32f5d6992b4e64cf7c0e23514
Carlos1200991 <1200991@isep.ipp.pt>
2022-06-04 17:26:09 +0100

    added spawning

--- GameJam2022/Assets/Scripts/Manager.cs
+++ GameJam2022/Assets/Scripts/Manager.cs
@@ -7,8 +7,8 @@ public class Manager : MonoBehaviour
 	public GameObject enemy;
 	public Transform Min;
 	public Transform Max;
-
 	public int score = 0;
+
 	private int score_per_second = 500;
 	private int score_per_enemy = 20000;
 
@@ -35,7 +35,7 @@ public class Manager : MonoBehaviour
 
 	private void Update()
 	{
-		score = (int)(Time.deltaTime * score_per_second);
+		score += (int)(Time.deltaTime * score_per_second);
 	}
 
 	public void enemyDeath()
@@ -45,7 +45,26 @@ public class Manager : MonoBehaviour
 
 	private void Spawn_Enemies()
 	{
-		Instantiate(enemy, new Vector2(Random.Range(Min.position.x, Max.position.x), Random.Range(Min.position.y, Max.position.y)), Quaternion.identity);
+		bool found = true;
+		Vector2 spawnPosition = new Vector2(0,0);
+
+		while (found)
+        {
+			found = false;
+			spawnPosition = new Vector2(Random.Range(Min.position.x, Max.position.x), Random.Range(Min.position.y, Max.position.y));
+
+			foreach (Collider2D nearbyObject in Physics2D.OverlapCircleAll(spawnPosition, 0.5f))	
+			{
+				if (nearbyObject.gameObject.CompareTag("Player"))
+				{
+					found = true;
+				}
+			}
+
+		}
+
+		
+		Instantiate(enemy, spawnPosition, Quaternion.identity);
 	}
 }
 
